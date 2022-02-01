26e494a58849c2f74a7a1f7d36db97715346a88e
0x000eHx <51081164+0x000eHx@users.noreply.github.com>
2022-02-01 13:25:10 +0100

    find tiles inside the geometry

--- gridding_spacial_geometries.py
+++ gridding_spacial_geometries.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 from pathlib import Path
+from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
 import shapely
 import numpy as np
@@ -18,8 +19,8 @@ if __name__ == '__main__':
     xmin, ymin, xmax, ymax = gdf_talsperre.total_bounds
 
     # real cell dimensions
-    cell_width = 0.00001  # 0.0817750
-    cell_height = 0.00001  # 0.0321233
+    cell_width = 0.0001  # 0.0817750
+    cell_height = 0.0001  # 0.0321233
 
     grid_cells = []
     talsperre_bounds = gdf_talsperre.geometry
@@ -27,12 +28,13 @@ if __name__ == '__main__':
     talsperre_gdf_exploded = gdf_talsperre.geometry.explode().tolist()
     max_area_talsperre = max(talsperre_gdf_exploded, key=lambda a: a.area)
 
-    for x0 in np.arange(xmin, xmax + cell_width, cell_width):
+    for x0 in tqdm(np.arange(xmin, xmax + cell_width, cell_width)):
         for y0 in np.arange(ymin, ymax + cell_height, cell_height):
             x1 = x0 - cell_width
             y1 = y0 + cell_height
             new_cell = shapely.geometry.box(x0, y0, x1, y1)
-            if new_cell.intersects(max_area_talsperre):
+            # if new_cell.intersects(max_area_talsperre):
+            if new_cell.within(max_area_talsperre):
                 grid_cells.append(new_cell)
             else:
                 pass
