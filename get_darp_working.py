a0c0d2efa07eaf4d0ccafcff577267a605c7b238
0x000eHx <51081164+0x000eHx@users.noreply.github.com>
2022-06-03 00:03:13 +0200

    cleaning STC and Paths construction from folium drawing annd fixed GeoDataframe endless appending of already constructed subcells and paths

--- get_darp_working.py
+++ get_darp_working.py
@@ -43,14 +43,6 @@ if __name__ == '__main__':
     grid_gdf = gpd.read_file(filename=f'./geodataframes/{last_file_no_suffix}.geojson')
     print(f'Loaded grid file: ./geodataframes/{last_file_no_suffix}.geojson')
 
-    # draw loaded map in browser and save
-    fol_map = grid_gdf.explore('covered_area', cmap='Spectral')  # YlGn,jet, PuBu, legend=True, scheme='quantiles'
-    for sp in settings['real_start_points']:
-        folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(fol_map)
-    fol_map.save(f'htmls/{last_file_no_suffix}_grid.html')
-    path = f'file:///{os.getcwd()}/htmls/{last_file_no_suffix}_grid.html'
-    webbrowser.open(path)
-
     list_real_start_points_coords = settings['real_start_points']
     export_file_name = generate_file_name(settings['geojson_file_name'])
 
@@ -76,7 +68,7 @@ if __name__ == '__main__':
             for i, point_tuple in enumerate(start_points):
                 dict_darp_startparameters[i] = {'row': point_tuple[0],
                                                 'col': point_tuple[1],
-                                                'tiles_count': 100}
+                                                'tiles_count': 150}
 
             # settings['darp_random_seed_value']
             handle = MultiRobotPathPlanner(np_bool_array, settings['darp_max_iter'], settings['darp_cc_variation'],
@@ -85,22 +77,18 @@ if __name__ == '__main__':
                                            settings['darp_trigger_importance'], False,
                                            settings['trigger_image_export_final_assignment_matrix'],
                                            settings['trigger_video_export_assignment_matrix_changes'],
-                                           export_file_name)  # TODO a real name for every grid of tile_size x
+                                           f'{export_file_name}_{str(geoserie.tiles_group_identifier)}')  # TODO a real name for every grid of tile_size x
             if handle.darp_success:
                 gdf_path_one_multipoly = generate_stc_geodataframe(gdf_numpy_positions, handle.darp_instance.A,
                                                                    handle.best_case.paths,
                                                                    geoserie.tiles_group_identifier)
-                gdf_subcells_and_lines_collection = gpd.GeoDataFrame(pandas.concat([gdf_subcells_and_lines_collection,
-                                                                                    gdf_path_one_multipoly], axis=0,
-                                                                                   ignore_index=True),
-                                                                     crs=gdf_path_one_multipoly.crs)
 
                 # filter for lines only and try to unify them
-                list_all_startpoints = gdf_subcells_and_lines_collection[gdf_subcells_and_lines_collection['line']].assigned_startpoint.unique()
+                list_all_startpoints = gdf_path_one_multipoly[gdf_path_one_multipoly['line']].assigned_startpoint.unique()
 
-                for idx, startpoint in enumerate(list_all_startpoints):
-                    bla = gdf_subcells_and_lines_collection.query(f'line == True and assigned_startpoint == {startpoint}')
-                    path_multilinestring = linemerge(bla.geometry.to_list())
+                for idx_point, startpoint in enumerate(list_all_startpoints):
+                    temp = gdf_path_one_multipoly.query(f'line == True and assigned_startpoint == {startpoint}')
+                    path_multilinestring = linemerge(temp.geometry.to_list())
                     # path_multilinestring = make_valid(unary_union(merged_lines))
                     print("Unified path lines!")
 
@@ -110,31 +98,22 @@ if __name__ == '__main__':
                              'path_length_meter': path_multilinestring.length,
                              'geometry': path_multilinestring}]
 
-                    gs_one_multipoly_path = gpd.GeoDataFrame(data, crs=4326).set_geometry('geometry')
+                    gdf_one_multipoly_path = gpd.GeoDataFrame(data, crs=4326).set_geometry('geometry')
                     gdf_path_per_multipoly = gpd.GeoDataFrame(pandas.concat([gdf_path_per_multipoly,
-                                                                             gs_one_multipoly_path], axis=0,
+                                                                             gdf_one_multipoly_path], axis=0,
                                                                             ignore_index=True),
                                                               crs=4326)
+                # finally append everything
+                gdf_subcells_and_lines_collection = gpd.GeoDataFrame(pandas.concat([gdf_subcells_and_lines_collection,
+                                                                                    gdf_path_one_multipoly], axis=0,
+                                                                                   ignore_index=True),
+                                                                     crs=4326)
+
         # save results to file before drawing
         gdf_subcells_and_lines_collection.to_file(
             filename=f'./geodataframes/{export_file_name}_subcells_and_lines_collection.geojson', driver="GeoJSON")
         gdf_path_per_multipoly.to_file(filename=f'./geodataframes/{export_file_name}_path_per_tilegroup.geojson',
                                        driver="GeoJSON")
-
-        # draw loaded map in browser and save
-        fol_map = gdf_subcells_and_lines_collection.explore('assigned_startpoint', scheme='quantiles', cmap='YlGn')  # YlGn,jet, PuBu, legend=True, scheme='quantiles'
-        for sp in settings['real_start_points']:
-            folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(fol_map)
-        fol_map.save(f'htmls/subcells_with_paths.html')
-        path = 'file:///' + os.getcwd() + '/htmls/subcells_with_paths.html'
-        webbrowser.open(path)
-
-        paths_map = gdf_path_per_multipoly.explore('sensor_line_length_meter', cmap='PuBu')  # YlGn,jet, PuBu, legend=True, scheme='quantiles'
-        for sp in settings['real_start_points']:
-            folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(paths_map)
-        paths_map.save(f'htmls/paths_only.html')
-        path = 'file:///' + os.getcwd() + '/htmls/paths_only.html'
-        webbrowser.open(path)
 
         measure_end = time.time()
         print("Elapsed time path generation (with darp): ", str((measure_end - measure_start) / 60), "min")
