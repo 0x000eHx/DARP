e2ab9cb995770bb740f31a973955b6b32db43202
0x000eHx <51081164+0x000eHx@users.noreply.github.com>
2022-06-12 14:49:27 +0200

    extend results display to show stc tiles

--- display_results.py
+++ display_results.py
@@ -1,8 +1,6 @@
 import sys
 from pathlib import Path
 import os
-import pandas
-from tqdm.auto import tqdm
 import geopandas as gpd
 import webbrowser
 from setting_helpers import load_yaml_config_file
@@ -28,31 +26,34 @@ if __name__ == '__main__':
     subcells_and_lines_collection_file = search_newest_file_in_folder(Path('geodataframes'), 'subcells_and_lines_collection')
     subcells_and_lines_collection_gdf = gpd.read_file(filename=f'./geodataframes/{subcells_and_lines_collection_file}.geojson')
 
+    # set geometry for drawing
     paths_gdf.set_geometry('geometry').set_crs(crs=4326)
     subcells_and_lines_collection_gdf.set_geometry('geometry').set_crs(crs=4326)
 
-    # draw loaded map in browser and save
-    paths_map = paths_gdf.explore('path_length_meter', scheme='quantiles', cmap='jet')  # YlGn,jet,Spectral, PuBu, legend=True, scheme='quantiles'
-    subcells_map = subcells_and_lines_collection_gdf.explore(column='tiles_group_identifier', cmap='Spectral')
-
+    # save paths_only map as html and draw in browser
+    paths_map = paths_gdf.explore('path_length_meter', cmap='Spectral')  # YlGn,jet,Spectral,PuBu, legend=True, scheme='quantiles'
     for sp in settings['real_start_points']:
         folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(paths_map)
     paths_map.save(f'htmls/paths_only.html')
     path = 'file:///' + os.getcwd() + '/htmls/paths_only.html'
     webbrowser.open(path)
 
+    # save subcells_with_paths map as html and draw in browser
+    subcells_map = subcells_and_lines_collection_gdf.explore(column='tiles_group_identifier', cmap='Spectral')
     for sp in settings['real_start_points']:
         folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(subcells_map)
     subcells_map.save(f'htmls/subcells_with_paths.html')
     path = 'file:///' + os.getcwd() + '/htmls/subcells_with_paths.html'
     webbrowser.open(path)
 
-    # draw loaded map in browser and save
-    # fol_map = grid_gdf.explore('covered_area', cmap='Spectral')  # YlGn,jet, PuBu, legend=True, scheme='quantiles'
-    # for sp in settings['real_start_points']:
-    #     folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(fol_map)
-    # fol_map.save(f'htmls/{last_file_no_suffix}.html')
-    # path = f'file:///{os.getcwd()}/htmls/{last_file_no_suffix}.html'
-    # webbrowser.open(path)
+    # save grid_gdf map as html and draw in browser
+    grid_file = search_newest_file_in_folder(Path('geodataframes'), 'grid')
+    grid_gdf = gpd.read_file(filename=f'./geodataframes/{grid_file}.geojson')
+    fol_map = grid_gdf.explore('covered_area', cmap='Spectral')  # YlGn,jet, PuBu, legend=True, scheme='quantiles'
+    for sp in settings['real_start_points']:
+        folium.Marker([sp[1], sp[0]], popup="<i>Startpoint</i>").add_to(fol_map)
+    fol_map.save(f'htmls/{grid_file}.html')
+    path = f'file:///{os.getcwd()}/htmls/{grid_file}.html'
+    webbrowser.open(path)
 
     sys.exit(0)
