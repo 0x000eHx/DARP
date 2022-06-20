050ab48a487e2131b99e5440b36ec3882e184c2d
0x000eHx <51081164+0x000eHx@users.noreply.github.com>
2022-06-21 00:14:10 +0200

    WIP small task manager rework and started grid generation matching the task manager input

--- get_grid.py
+++ get_grid.py
@@ -1,16 +1,7 @@
 import sys
-from gridding_helpers import check_edge_length_polygon_threshold, find_grid, get_biggest_area_polygon
+from gridding_helpers import generate_file_name, generate_grid, get_biggest_area_polygon, Grid_Generation_Task_Manager
 import time
 from setting_helpers import load_yaml_config_file, write_yaml_config_file
-
-
-def generate_file_name(filename: str):
-    export_file_name = f'{time.strftime("%Y-%m-%d_%H-%M-%S")}_{str(filename)}'
-    # Replace all characters in dict
-    b = {' ': '', '.geojson': ''}
-    for x, y in b.items():
-        export_file_name = export_file_name.replace(x, y)
-    return export_file_name
 
 
 if __name__ == '__main__':
@@ -18,27 +9,28 @@ if __name__ == '__main__':
     write_yaml_config_file(settings_yaml_filepath)
     settings = load_yaml_config_file(settings_yaml_filepath)
 
-    if check_edge_length_polygon_threshold(settings['sensor_line_length_meter'], settings['polygon_threshold']):
+    # find the Shapely Geometry (Multipolygon) of interest
+    area_polygon = get_biggest_area_polygon(settings['geojson_file_name'])
 
-        measure_start = time.time()
+    # create a task manager
+    task_manager = Grid_Generation_Task_Manager(settings['sensor_line_length_meter'], settings['polygon_threshold'], area_polygon, settings['geojson_file_name'])
+    # Grid generation will be generic without any specification
+    # the widest scanner line will get used to create the biggest grid
+    # use the Notebook if you wanna specify the regions per scanner line width
 
-        area_polygon = get_biggest_area_polygon(settings['geojson_file_name'])
+    measure_start = time.time()
 
-        # find biggest grid of highest value in sensor_line_length_meter
-        grid_gdf = find_grid(area_polygon, settings['sensor_line_length_meter'], settings['polygon_threshold'])
+    # find biggest grid of highest value in sensor_line_length_meter
+    grid_gdf = generate_grid(task_manager)
 
-        if not grid_gdf.empty:
-            # save best results
-            file_name = generate_file_name(settings['geojson_file_name'])
-            grid_gdf.to_file(filename=f'./geodataframes/{file_name}_grid.geojson', driver="GeoJSON")
-            print("Successfully finished grid generation and saved geometry to file!\n",
-                  f'./geodataframes/{file_name}_grid.geojson')
+    if not grid_gdf.empty:
+        # save best results
+        file_name = generate_file_name(settings['geojson_file_name'])
+        grid_gdf.to_file(filename=f'./geodataframes/{file_name}_grid.geojson', driver="GeoJSON")
+        print("Successfully finished grid generation and saved geometry to file!\n",
+              f'./geodataframes/{file_name}_grid.geojson')
 
-        measure_end = time.time()
-        print("Elapsed time grid generation: ", (measure_end - measure_start), "sec")
-
-    else:
-        print("check_edge_length_polygon_threshold() or check_real_start_points() failed!")
-        sys.exit(13)
+    measure_end = time.time()
+    print("Elapsed time grid generation: ", (measure_end - measure_start), "sec")
 
     sys.exit(0)
